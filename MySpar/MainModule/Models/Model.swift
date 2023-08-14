c0cfdcd3b7c5ef7af5fac6c462987c51e5c36748
Novgorodtcev <82874611+Berendei75405@users.noreply.github.com>
2023-08-15 04:07:21 +0700

    Add files via upload

---
+++ MySpar/MainModule/Models/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  MySpar
+//
+//  Created by Новгородцев Никита on 14.08.2023.
+//
+
+import Foundation
+
+struct Product {
+    let photo: String
+    let price: Float
+    let oldPrice: Float
+    let category: String
+    let discount: Int
+    let weightPhoto: String
+}
